511304dc4f978723bec52fdd05b62f2eeaf6b65b
NeDimaN <dn.nenashev@yandex.ru>
2022-01-11 18:07:22 +0400

    save 11.01.2022

--- work_area_4.py
+++ work_area_4.py
@@ -591,16 +591,53 @@
 #         self.num = num  # номер счета
 #         self.percent = percent  # процент начисления
 #         self.value = value  # сумма в рублях
-#         print(f'Счет #{num} принадлежащий {surname} был открыт.')
+#         print(f'Счет #{self.num} принадлежащий {self.surname} был открыт.')
 #         print('*' * 50)
+#
+#     def __del__(self):
+#         print('*' * 50)
+#         print(f'Счет #{self.num} принадлежащий {self.surname} был закрыт.')
 #
 #     @staticmethod
 #     def convert(value, rate):
 #         return value * rate
 #
+#     @classmethod
+#     def set_usd_rate(cls, rate):  # редактирование рубля по отношению к доллару
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):  # редактирование рубля по отношению к евро
+#         cls.rate_euro = rate
+#
+#     def add_percents(self):  # начисление процентов
+#         self.value += self.value * self.percent
+#         print('Проценты были успешно начислены!')
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):# снятие заданной суммы
+#         if val > self.value:
+#             print(f'к сожалению у вас нет {val} {Account.suffix}')
+#         else:
+#             self.value -= val
+#             print(f'{val} {Account.suffix} ,было успешно снято')
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.value += val
+#         print(f'{val} {Account.suffix} ,было успешно добавлено!')
+#         self.print_balance()
+#
+#     def edit_owner(self, surname):  # смена владельца счета
+#         self.surname = surname
+#
 #     def convert_to_usd(self):  # перевод в доллары
 #         usd_val = Account.convert(self.value, Account.rate_usd)
 #         print(f'Состояние счета: {usd_val} {Account.suffix_usd}.')
+#
+#     def convert_to_eur(self):  # перевод в евро
+#         eur_val = Account.convert(self.value, Account.rate_euro)
+#         print(f'Состояние счета: {eur_val} {Account.suffix_eur}.')
 #
 #     def print_balance(self):
 #         print(f'Текущий баланс {self.value} {Account.suffix}')
@@ -618,63 +655,137 @@
 # acc = Account('Ненашев', 12345, 0.03, 1000)
 # acc.print_info()
 # acc.convert_to_usd()
+# acc.convert_to_eur()
+#
+# Account.set_usd_rate(2)
+# Account.set_eur_rate(3)
+# print()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+# acc.edit_owner('Дюма')
+# acc.print_info()
+#
+# acc.add_percents()
+# print()
+# acc.withdraw_money(3000)
+# print()
+# acc.withdraw_money(100)
+# print()
+# acc.add_money(5000)
+# print()
+# acc.withdraw_money(3000)
+# print()
 
 
-class Point:
-
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f'{self.x}, {self.y}'
-
-
-class Prop:
-    def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
-        print('Инициализатор базового класса Prop')
-        self.sp = sp
-        self.ep = ep
-        self.color = color
-        self.__width = width
-
-    def get_width(self):
-        return self.__width
-
-
-class Line(Prop):
-    def __init__(self, *args):
-        print('Переопределенный инициализатор Line')
-        # Prop.__init__(self, *args)
-        super().__init__(*args)
-        self.__width = 5
-    # def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
-    #     self.sp = sp
-    #     self.ep = ep
-    #     self.color = color
-    #     self.width = width
-
-    def draw_line(self):
-        print(f'Рисование линии: {self.sp}, {self.ep}, {self.color}, {self.get_width()}')
-
-
-class Rect(Prop):
-    # def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
-    #     self.sp = sp
-    #     self.ep = ep
-    #     self.color = color
-    #     self.width = width
-
-    def draw_rect(self):
-        print(f'Рисование прямоугольника: {self.sp}, {self.ep}, {self.color}, {self.get_width()}')
-
-
-line = Line(Point(1, 2), Point(10, 20))
-line.draw_line()
-print(line.__dict__)
+# class Point:
+#
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+#
+#     def __str__(self):
+#         return f'{self.x}, {self.y}'
+#
+#
+# class Prop:
+#     def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
+#         print('Инициализатор базового класса Prop')
+#         self.sp = sp
+#         self.ep = ep
+#         self.color = color
+#         self.__width = width
+#
+#     def get_width(self):
+#         return self.__width
+#
+#
+# class Line(Prop):
+#     def __init__(self, *args):
+#         print('Переопределенный инициализатор Line')
+#         # Prop.__init__(self, *args)
+#         super().__init__(*args)
+#         self.__width = 5
+#     # def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
+#     #     self.sp = sp
+#     #     self.ep = ep
+#     #     self.color = color
+#     #     self.width = width
+#
+#     def draw_line(self):
+#         print(f'Рисование линии: {self.sp}, {self.ep}, {self.color}, {self.get_width()}')
+#
+#
+# class Rect(Prop):
+#     # def __init__(self, sp: Point, ep: Point, color: str = 'red', width: int = 1):
+#     #     self.sp = sp
+#     #     self.ep = ep
+#     #     self.color = color
+#     #     self.width = width
+#
+#     def draw_rect(self):
+#         print(f'Рисование прямоугольника: {self.sp}, {self.ep}, {self.color}, {self.get_width()}')
+#
+#
+# line = Line(Point(1, 2), Point(10, 20))
+# line.draw_line()
+# print(line.__dict__)
 
 # line.width = 10
 # print(line.width)
 # line.draw_line()
 # rect = Rect(Point(30, 40), Point(7, 86))
 # rect.draw_rect()
+
+class Figure:
+    def __init__(self, color):
+        self.__color = color
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, c):
+        self.__color = c
+
+
+class Rectangle(Figure):
+    def __init__(self, width, height, color, border):
+        super().__init__(color)
+        self.__width = width
+        self.__height = height
+        self.border = border
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        if w > 0:
+            self.__width = w
+        else:
+            raise ValueError('Значение ширины должно быть больше 0!')
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        if h > 0:
+            self.__height = h
+        else:
+            raise ValueError('Значение высоты должно быть больше 0!')
+
+    def border_new(self, b):
+        self.border = b
+
+    def area(self):
+        return self.__width * self.__height
+        # return self.border_new('1px solid gray')
+
+
+rect = Rectangle(10, 20, 'green')
+print(rect.area())
