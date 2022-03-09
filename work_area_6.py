a2c6cd1961d1311380687f8cb1bcbddc1b045ef2
NeDimaN <dn.nenashev@yandex.ru>
2022-03-09 14:17:11 +0400

    save 09.03.2022

--- work_area_6.py
+++ work_area_6.py
@@ -10,11 +10,11 @@
 
 
 
-import sqlite3 as sq
-
-with sq.connect('profile.db') as con:
-    cur = con.cursor()
-    cur.execute('DROP TABLE users')
+# import sqlite3 as sq
+#
+# with sq.connect('profile.db') as con:
+#     cur = con.cursor()
+#     cur.execute('DROP TABLE users')
     # cur.execute("""
     # CREATE TABLE IF NOT EXISTS users(
     # id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,3 +26,43 @@ with sq.connect('profile.db') as con:
 
 
 
+#=====================================================03.03.2022
+
+import sqlite3 as sq
+
+# with sq.connect('users.db') as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     INSERT INTO person
+#     VALUES (10, 'Ольга', '79879755627', 25, 'test@mail.ru')
+#     """)
+#     # cur.execute("""
+#     # CREATE TABLE IF NOT EXISTS person(
+#     # id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     # name TEXT NOT NULL,
+#     # phone BLOB NOT NULL DEFAULT +79879750000,
+#     # age  INTEGER NOT NULL CHECK(age > 0 AND age <100),
+#     # email TEXT UNIQUE
+#     # )
+#     # """)
+#=================================
+
+with sq.connect('db_4.db') as con:
+    cur = con.cursor()
+    cur.execute("""
+    SELECT *
+    FROM Ware
+    ORDER BY Price DESC
+    LIMIT 2, 5;
+    """)
+
+    res = cur.fetchone()
+    print(res)
+    res2 = cur.fetchmany(3)
+    print(res2)
+
+    # for res in cur:
+    #     print(res)
+
+    # res = cur.fetchall()
+    # print(res)
